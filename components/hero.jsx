164972fed5ee4bcf964f3011de5ff481e35fad03
David Okpe <okpedavid0@gmail.com>
2025-07-11 20:55:51 +0100

    update

--- components/hero.jsx
+++ components/hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
                 <GradientButton 
                 text='Resume' 
                 icon={<LucideFileText />} 
-                link='https://drive.google.com/uc?export=download&id=13fiqHR7M3mTQ20Wi14OU21IuVXx4zj-9' />
+                link='https://drive.google.com/uc?export=download&id=1Rn3qemO5_hRk9IEOqHDZgH4RNbW3bZ5T' />
         </div>
         
     </section>
